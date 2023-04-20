24a36947df0af6ce37e176793495c805f0e498a2
mdjewel999 <mdjewel878761@gmail.com>
2023-04-21 03:17:45 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyATB2-g79vU-MV6PH054vEGma-qGBY4oX8",
+  authDomain: "the-news-dragon-41986.firebaseapp.com",
+  projectId: "the-news-dragon-41986",
+  storageBucket: "the-news-dragon-41986.appspot.com",
+  messagingSenderId: "338684572983",
+  appId: "1:338684572983:web:f056288ac7326fb88e96b2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/pages/Shared/Header/Header.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import logo from "../../../assets/logo.png";
+import moment from "moment";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
+
+const Header = () => {
+  return (
+    <Container className="mt-4">
+      <div className="text-center">
+        <img src={logo} alt=""/>
+        <p>
+          <small className="text-secondary">
+            Journalism Without Fear or Favour
+          </small>
+        </p>
+        <p>{moment().format("dddd, MMMM D, YYYY")}</p>
+      </div>
+      <div className="d-flex">
+        <Button variant="danger">Latest</Button>
+        <Marquee className="text-danger" speed={80}>
+        Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...
+        </Marquee>
+      </div>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+      <Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+            <Nav.Link href="#pricing">career</Nav.Link>
+        
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Profile</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+            <Button variant="secondary">Login</Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </Container>
+  );
+};
+
+export default Header;
