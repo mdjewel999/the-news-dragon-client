e9aa8753806db7c0c357b03c004630c4d9777e03
mdjewel999 <mdjewel878761@gmail.com>
2023-04-29 01:04:20 +0600

    last code

--- src/layouts/NewsLayout.jsx
+++ src/layouts/NewsLayout.jsx
@@ -5,20 +5,16 @@ import { Col, Container, Row } from "react-bootstrap";
 import LeftNav from "../pages/Shared/LeftNav/LeftNav";
 import RightNav from "../pages/Shared/RightNav/RightNav";
 import { Outlet } from "react-router-dom";
-import NavigationBar from "../pages/Shared/NavigationBar/NavigationBar";
 
-const Main = () => {
+const NewsLayout = () => {
   return (
     <div>
       <Header></Header>
-    <NavigationBar></NavigationBar>
+
       <Container>
-       
         <Row>
-          <Col lg={3}>
-            <LeftNav></LeftNav>
-          </Col>
-          <Col lg={6}>
+        
+          <Col lg={9}>
             <Outlet></Outlet>
           </Col>
           <Col lg={3}>
@@ -31,4 +27,4 @@ const Main = () => {
   );
 };
 
-export default Main;
+export default NewsLayout;
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import logo from "../../../assets/logo.png";
 import moment from "moment";
-import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import Marquee from "react-fast-marquee";
 
+
 const Header = () => {
+
+
   return (
     <Container className="mt-4">
       <div className="text-center">
@@ -18,29 +21,20 @@ const Header = () => {
       </div>
       <div className="d-flex">
         <Button variant="danger">Latest</Button>
-        <Marquee className="text-danger" speed={80}>
-        Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...
+        <Marquee className="text-danger" speed={25}>
+        Experts warn of new cyber attack targeting global financial institutions
+Study finds significant progress in cancer research, leading to potential breakthroughs
+World Health Organization announces global initiative to combat new strain of flu
+UN peacekeeping mission extends mandate in conflict-ridden region
+Tech giants face new antitrust investigations over alleged monopolistic practices
+Sports fans outraged as major league baseball players strike for better pay
+Climate change worsens as Arctic sea ice reaches record low for third consecutive year
+Major airlines announce plans to launch supersonic commercial flights
+Pop star's latest album breaks streaming records, sets new industry standards
+World leaders convene for summit on global economic recovery and trade agreements.
         </Marquee>
       </div>
-      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-      <Container>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mx-auto">
-            <Nav.Link href="#features">Home</Nav.Link>
-            <Nav.Link href="#pricing">About</Nav.Link>
-            <Nav.Link href="#pricing">career</Nav.Link>
-        
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">Profile</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-            <Button variant="secondary">Login</Button>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+   
     </Container>
   );
 };
